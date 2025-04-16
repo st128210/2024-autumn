e2b6c908be9093e6e19207b2030d17d06b6a23a8
st128210 <st128210@student.spbu.ru>
2025-04-16 22:03:43 +0300

    HW upload

--- 2025.02.28-Homework01/2025.02.28-Homework01/Source.cpp
+++ 2025.02.28-Homework01/2025.02.28-Homework01/Source.cpp
@@ -9,80 +9,35 @@ private:
 	{
 		this->len = len;
 		this->data = (int*)malloc(sizeof(int) * len);
+		if (this->data != nullptr)
+		{
+			for (int i = 0; i < len; ++i)
+			{
+				set(i, 0);
+			}
+		}
 	}
 	bool indexValid(int index)
 	{
 		return 0 <= index < this->len;
 	}
-	void expand()
+	void expand(int added = 1)
 	{
-		int* a = (int*)malloc(sizeof(int) * len + 1);
-		for (int i = 0; i < len; ++i)
+		int newlen = this->len + added;
+		int* newdata = (int*)malloc(sizeof(int) * newlen);
+		if (newdata != nullptr)
 		{
-			a[i] = data[i];
+			for (int i = 0; i < this->len; ++i)
+			{
+				newdata[i] = get(i);
+			}
 		}
-		ArrayList(len + 1);
-		for (int i = 0; i < len; ++i)
+		if (this->data != nullptr)
 		{
-			data[i] = a[i];
+			free(this->data);
+			this->data = newdata;
 		}
-		++len;
-		free(a);
-	}
-
-	void swap(int i1, int i2)
-	{
-		int c = get(i1);
-		set(i1, get(i2));
-		set(i2, c);
-	}
-
-public:
-	ArrayList(int len = 10)
-	{
-		this->init(len);
-		for (int i = 0; i < this->len; ++i)
-		{
-			this->data[i] = 0;
-		}
-	}
-	ArrayList()
-	{
-		free(this->data);
-	}
-	void randomize(int min = 10, int max = 99)
-	{
-		for (int i = 0; i < len; ++i)
-		{
-			this->data[i] = rand() % (max - min + 1) + min;
-		}
-	}
-	void print()
-	{
-		for (int i = 0; i < len; ++i)
-		{
-			printf("%d ", this->data[i]);
-		}
-		printf("\n");
-	}
-	int get(int index)
-	{
-		if (indexValid(index))
-		{
-			return this->data[index];
-		}
-		return -1;
-	}
-	void set(int index, int value)
-	{
-		if (indexValid(index))
-		{
-			this->data[index] = value;
-		}
-	}
-	int count()
-	{
-		return len;
+		this->len = newlen;
 	}
 	void contract(int reduced = 1)
 	{
@@ -106,7 +61,73 @@ public:
 			this->len = newlen;
 		}
 	}
+	void swap(int i1, int i2)
+	{
+		int c = get(i1);
+		set(i1, get(i2));
+		set(i2, c);
+	}
 
+public:
+	ArrayList(int len = 10)
+	{
+		this->init(len);
+	}
+	ArrayList(ArrayList& list)
+	{
+		this->init(list.len);
+		for (int i = 0; i < list.len; ++i)
+		{
+			this->set(i, list.get(i));
+		}
+	}
+	/*~ArrayList()
+	{
+		if (this->data != nullptr)
+		{
+			for (int i = 0; i < this->len; ++i)
+			{
+				this->data[i] = 0;
+			}
+			free(this->data);
+			this->data = nullptr;
+			this->len = 0;
+		}
+	}*/
+	void randomize(int min = 10, int max = 99)
+	{
+		for (int i = 0; i < len; ++i)
+		{
+			data[i] = rand() % (max - min + 1) + min;
+		}
+	}
+	void print()
+	{
+		for (int i = 0; i < this->len; ++i)
+		{
+			printf("%d ", this->data[i]);
+		}
+		printf("\n");
+	}
+	int get(int index)
+	{
+		if (indexValid(index))
+		{
+			return data[index];
+		}
+		return -1;
+	}
+	void set(int index, int value)
+	{
+		if (this->indexValid(index))
+		{
+			set(index, value);
+		}
+	}
+	int count()
+	{
+		return len;
+	}
 	void pushBack(int element)
 	{
 		expand();
@@ -119,7 +140,7 @@ public:
 	}
 	void insert(int index, int element)
 	{
-		expand(); 
+		expand();
 		for (int i = len - 1; i > index; --i)
 		{
 			set(i, get(i - 1));
@@ -145,7 +166,6 @@ public:
 			set(i, get(i + 1));
 		}
 		contract();
-		return res;
 	}
 	void reverse(int start, int end)
 	{
@@ -161,62 +181,55 @@ public:
 	}
 	int sum()
 	{
-		int sum = 0;
+		int res = 0;
 		for (int i = 0; i < len; ++i)
 		{
-			sum += get(i);
+			res += get(i);
 		}
-		return sum;
+		return res;
 	}
 	/// <summary>
 	/// Второй по величине элемент
 	/// </summary>
-	int secondMaX()
+	int secondMax()
 	{
 		int max1 = get(0);
 		int max2 = get(0);
-		for (int i = 0; i < len; ++i)
+		for (int i = 0; i < this->len; ++i)
 		{
-			if (max1 < get(i))
+			if (max1 <= get(i))
 			{
 				max2 = max1;
 				max1 = get(i);
 			}
-			if (max2 < get(i))
+			else if (max2 <= get(i))
 			{
 				max2 = get(i);
 			}
 		}
-		return max2;
 	}
 	/// <summary>
 	/// индекс последнего минимального элемента
 	/// </summary>
 	int lastMinIndex()
 	{
-		int LastMinIndex = 0;
-		for (int i = 0; i < len; ++len)
+		int MinIndex = 0;
+		for (int i = 0; i < len; ++i)
 		{
-			if (get(i) <= get(LastMinIndex))
+			if (get(i) <= get(MinIndex))
 			{
-				LastMinIndex = i;
+				MinIndex = i;
 			}
 		}
-		return LastMinIndex;
+		return MinIndex;
 	}
-	/// <summary>
-	/// сместить на k элементов вправо
-	/// </summary>
-	void shift(int k)
-	{
-		reverse(0, len - k - 1);
-		reverse(len - k, len - 1);
-		reverse(0, len - 1);
+	int shift(int k)					// 3
+	{									// 0 1 2 3 4 5 6 7 8
+		reverse(0, len - k - 1);		// 5 4 3 2 1 0 6 7 8 
+		reverse(len - k, len - 1);		// 5 4 3 2 1 0 8 7 6  
+		reverse(0, len - 1);			// 6 7 8 0 1 2 3 4 5
 	}
-	/// <summary>
-	/// количество нечетных
-	/// </summary>
-	int countOdd(int len)
+	int countOdd()
 	{
 		int count = 0;
 		for (int i = 0; i < len; ++i)
@@ -228,24 +241,19 @@ public:
 		}
 		return count;
 	}
-	/// <summary>
-	/// сумма четных элементов
-	/// </summary>
-	int sumEven(int len)
+	int sumEven()
 	{
 		int sum = 0;
 		for (int i = 0; i < len; ++i)
 		{
 			if (get(i) % 2 == 0)
 			{
-				sum += data[i];
+				sum += get(i);
 			}
 		}
 		return sum;
 	}
-
 };
-
 int max(ArrayList list)
 {
 	int max = list.get(0);
@@ -261,10 +269,10 @@ int max(ArrayList list)
 
 int main(int argc, char* argv[])
 {
-	//ArrayList list(10);
-	/*list.randomize();
-	list.print();
-	printf("%d\n", max(list));*/
-	std::cout << "Hello Biden";
+	ArrayList array;
+	array.randomize();
+	array.sumEven();
+	array.countOdd();
+	array.print();
 	return 0;
 }
